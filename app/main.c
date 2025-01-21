d1d7dda7de6bf4b4a79370bf98310dd7b05808a2
Anirudh Rathore <tdr11333@gmail.com>
2025-01-21 18:08:33 +0530

    Added output

--- app/main.c
+++ app/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -10,6 +10,7 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
-  printf("%c: command not found",input);
+  input[strlen(input)-1]='\0';
+  printf("%s: command not found\n",input);
   return 0;
 }
