692c714ea6615e322d933f926776e8a07a5278af
Anirudh Rathore <tdr11333@gmail.com>
2025-01-22 21:51:01 +0530

    added exit

--- app/main.c
+++ app/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
-in#t main() {
+#include <string.h>
+int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-
-  // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
+  while(1){
+	  fgets(input, 100, stdin);
+	  if(strcmp(input,"exit 0")==0) return 0;
+	  printf("$ ");
+    	  input[strlen(input)-1]='\0';
+	  printf("%s: command not found\n",input);
+  }
   return 0;
-l
+}
 	
