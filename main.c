6c3cc7a5f20d78f1d151549f109d264c619a7625
Anirudh Rathore <tdr11333@gmail.com>
2025-01-22 23:04:32 +0530

    {commit_message}

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main() {
+  // Flush after every printf
+  setbuf(stdout, NULL);
+
+  char input[100];
+  while(1){	
+	  printf("$ ");
+	  fgets(input, 100, stdin);
+    	  input[strlen(input)-1]='\0';
+	  
+	  if(strcmp(input,"exit 0")==0) exit(0);
+
+	  if(strncmp(input,"echo",strlen("echo"))==0){
+		printf("%s\n",input+5);
+	  }
+	  else printf("%s: command not found\n",input);
+  }
+  return 0;
+}
