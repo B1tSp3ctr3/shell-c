5c989418717010f47f2b550710eb75c044da6605
Anirudh Rathore <tdr11333@gmail.com>
2025-01-23 11:21:59 +0530

    Updated type

--- app/main.c
+++ app/main.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -15,6 +16,7 @@ int main()
         "echo",
         "type",
         "exit"};
+    char *path = getenv("PATH");
     input[strlen(input) - 1] = '\0';
     // exit command
     if (strcmp(input, "exit 0") == 0)
@@ -24,7 +26,7 @@ int main()
     {
       char *arguments = input + 5;
       int found = 0;
-      for (int i = 0; i < sizeof(builtins)/20; i++)
+      for (int i = 0; i < sizeof(builtins) / 20; i++)
       {
         if (strcmp(builtins[i], arguments) == 0)
         {
@@ -34,7 +36,24 @@ int main()
         }
       }
       if (found == 0)
+      {
+        char *directory = strktok(path, ":");
+        while (directory != NULL)
+        {
+          char *command = malloc(strlen(directory) + strlen(arguments) + 2);
+          strcpy(command, directory);
+          strcat(command, "/");
+          strcat(command, arguments);
+          if (access(arguments, F_OK) == 0)
+          {
+            printf("%s is %s\n", arguments, command);
+            found = 1;
+            break;
+          }
+          directory = strtok(NULL, ":");
+        }
         printf("%s: not found\n", input + 5);
+      }
     }
     // echo command
     else if (strncmp(input, "echo ", strlen("echo ")) == 0)
